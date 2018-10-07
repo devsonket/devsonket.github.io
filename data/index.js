60903d645ae7f5df3ff73e4e58b7f1098001dcdd
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-10-07 18:05:59 +0530

    index data structure added

---
+++ data/index.js
@@ -0,0 +1,42 @@
+[
+  {
+    id: 'react',
+    title: 'রিঅ্যাক্ট'
+  },
+  {
+    id: 'js',
+    title: 'জাভাস্ক্রিপ্ট'
+  },
+  {
+    id: 'git',
+    title: 'গিট'
+  },
+  {
+    id: 'github',
+    title: 'গিটহাব'
+  },
+  {
+    id: 'regex',
+    title: 'রেগুলার এক্সপ্রেশন'
+  },
+  {
+    id: 'ssh',
+    title: 'সিকিউর শেল'
+  },
+  {
+    id: 'gulp',
+    title: 'গাল্প'
+  },
+  {
+    id: 'others',
+    title: 'অন্যান্য'
+  },
+  {
+    id: 'new-another',
+    title: 'নতুন আরেকটা'
+  },
+  {
+    id: 'new',
+    title: 'নতুন একটা'
+  }
+]
