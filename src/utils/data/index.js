ee528364688bd2392c17c55f0e07797513ed36a1
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-10-07 18:05:06 +0530

    data organization

--- src/utils/data/index.js
+++ src/utils/data/index.js
@@ -1,12 +1,42 @@
-import react from './react';
-
-let data = [react];
-
-data = data.sort((a, b) => {
-  console.log(a);
-  console.log(b);
-})
-
-export default [
-  react
+[
+  {
+    id: 'react',
+    title: 'রিঅ্যাক্ট'
+  },
+  {
+    id: 'js',
+    title: 'জাভাস্ক্রিপ্ট'
+  },
+  {
+    id: 'git',
+    title: 'গিট'
+  },
+  {
+    id: 'github',
+    title: 'গিটহাব'
+  },
+  {
+    id: 'regex',
+    title: 'রেগুলার এক্সপ্রেশন'
+  },
+  {
+    id: 'ssh',
+    title: 'সিকিউর শেল'
+  },
+  {
+    id: 'gulp',
+    title: 'গাল্প'
+  },
+  {
+    id: 'others',
+    title: 'অন্যান্য'
+  },
+  {
+    id: 'new-another',
+    title: 'নতুন আরেকটা'
+  },
+  {
+    id: 'new',
+    title: 'নতুন একটা'
+  }
 ]
