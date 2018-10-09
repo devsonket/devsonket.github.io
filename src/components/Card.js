68641abe61605445d6345b1beb4041d0caf16e7c
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-10-09 13:47:52 +0530

    No description for top list

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
-export default({id, title, description = null, style = {}}) => (
+export default({id, title, style = {}}) => (
   <div key={id} className="card c-card" style={style}>
     <a href="/">
       <h4>{title}</h4>
-      <p>{description && description}</p>
     </a>
   </div>
 )
