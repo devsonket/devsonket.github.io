f164d808057c9636257af33de3a79d6f677768f7
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-11-14 22:05:29 +0530

    share and print functionality added

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import NavBar from './NavBar';
+import SocialShare from './SocialShare';
 
 export default({title, description}) => (
   <header className="header-area">
@@ -9,6 +10,10 @@ export default({title, description}) => (
       <div className="intro">
         <h1>{title}</h1>
         <p>{description}</p>
+        <div className="print no-print">
+          <button className="btn btn-print" onClick={() => window.print()}>প্রিন্ট করুন</button>
+        </div>
+        <SocialShare title={title} description={description} />
       </div>
     </div>
   </header>
