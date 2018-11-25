cda1e33418021bca62fcfdd6a5790fab3ba8663e
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-11-25 13:07:28 +0530

    new build

---
+++ precache-manifest.e09790a2c5f53430b31a87e21c6becc5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "13166c303931f86d93d2319a1aa15c7d",
+    "url": "/static/media/lost.13166c30.svg"
+  },
+  {
+    "revision": "ac52b429e985b36d88994f5a72647dea",
+    "url": "/static/media/Kalpurush.ac52b429.svg"
+  },
+  {
+    "revision": "797849bb979d05a914f3b88666c78c22",
+    "url": "/static/media/Kalpurush.797849bb.ttf"
+  },
+  {
+    "revision": "7268d6d360d4ec731abaea2f93deb19d",
+    "url": "/static/media/Kalpurush.7268d6d3.woff"
+  },
+  {
+    "revision": "1fa8c68dbc2271be80107d46e7567971",
+    "url": "/static/media/Kalpurush.1fa8c68d.eot"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "adcedd98c8a2843990f4",
+    "url": "/static/js/main.adcedd98.chunk.js"
+  },
+  {
+    "revision": "5ff573fc314a13e3dec0",
+    "url": "/static/js/1.5ff573fc.chunk.js"
+  },
+  {
+    "revision": "adcedd98c8a2843990f4",
+    "url": "/static/css/main.09ab1f14.chunk.css"
+  },
+  {
+    "revision": "9bf8a358e9c1c1de5ccd3e1d0664f05a",
+    "url": "/index.html"
+  }
+];
