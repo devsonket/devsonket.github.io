aaad8c33412c67c8af62e1157f1db0475f88b759
Zonayed Ahmed <zonayedpca@yahoo.com>
2018-11-27 14:38:18 +0530

    Contributor added

---
+++ src/utils/contributorMap.js
@@ -0,0 +1,17 @@
+export default data => {
+  const charMap = {};
+  data.forEach(oneData => {
+    const { login } = oneData.author;
+    if(!charMap[login]) {
+      charMap[login] = {
+        count: 1,
+        login: oneData.author.login,
+        avatar_url: oneData.author.avatar_url,
+        html_url: oneData.author.html_url
+      }
+    } else {
+      charMap[login].count++;
+    }
+  });
+  return charMap;
+}
