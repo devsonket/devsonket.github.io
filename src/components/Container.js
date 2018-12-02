b6772b2c426bd04a808098c6233fa8e144088961
iamruhul <ruhulamin3482@gmail.com>
2018-12-02 16:28:22 +0600

    extract style: container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
+import styled from "@emotion/styled";
 
-export default ({children}) => (
-  <section className="container">
-    {children}
-  </section>
-)
+const Container = styled.section`
+  max-width: 940px;
+  margin: 0 auto;
+  padding: 50px 0;
+
+  @media only screen and (max-width: 940px) {
+    width: 100%;
+  }
+
+  @media only screen and (max-width: 460px) {
+    padding: 25px 0;
+  }
+`;
+
+export default ({ children }) => <Container>{children}</Container>;
