fde892b207c265424931d32ec46175416df9bf71
iamruhul <ruhulamin3482@gmail.com>
2018-12-02 20:37:47 +0600

    ->emotion: contributor styles

--- src/components/HeaderContent.js
+++ src/components/HeaderContent.js
@@ -16,6 +16,42 @@ export const HeaderEl = styled.div`
 
 export const HeaderArea = styled.header`
   background-color: #eee;
+
+  .contributor {
+    list-style: none;
+    margin-bottom: 15px;
+  }
+
+  .contributor p {
+    margin-bottom: 10px;
+    font-weight: 900;
+  }
+
+  .contributor li {
+    display: inline-block;
+    position: relative;
+    margin: 0 5px;
+  }
+
+  .contributor-profile span {
+    position: absolute;
+    bottom: 5px;
+    right: -2px;
+    font-weight: bold;
+    background-color: #4caf50;
+    color: #fff;
+    font-size: 11px;
+    width: 15px;
+    height: 15px;
+    line-height: 15px;
+    border-radius: 40px;
+  }
+
+  .contributor-profile img {
+    width: 40px;
+    border-radius: 40px;
+    border: 3px solid #ffc107;
+  }
 `;
 
 const HeaderContent = props => {
