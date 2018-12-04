4abd2149e58f97335ef6c33936533624e5c047c6
Zonayed Ahmed <zonayedpcadotcom@gmail.com>
2018-12-04 21:25:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { hydrate, render } from 'react-dom';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
 const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement);
