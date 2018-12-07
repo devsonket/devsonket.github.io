5735670e5927e2d8315b28831844fa3e15510252
iamruhul <ruhulamin3482@gmail.com>
2018-12-07 21:57:52 +0600

    feature(smooth-navbar): smooth animation complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import styled from "@emotion/styled";
 
 import { BrandLogo } from "./BrandLogo";
@@ -10,13 +10,16 @@ const Navbar = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 0px 30px;
-  background: white;
+  background: rgba(255, 255, 255, 0.94);
   height: 60px;
-  box-shadow: 0 1px 2px 0 rgba(60, 64, 67, 0.3),
-    0 2px 6px 2px rgba(60, 64, 67, 0.15);
-
-  position: relative;
+  position: fixed;
   z-index: 999;
+  width: 100%;
+  border-bottom: 1px solid gainsboro;
+  top: 0;
+  z-index: 999;
+  transition: .2s all cubic-bezier(0, 0.94, 1, 0.99);
+  transform: ${props => (props.show ? `translateY(0)` : `translateY(-100%)`)};
 
   @media (max-width: 459px) {
     padding: 0px 10px;
@@ -73,11 +76,59 @@ const NewButton = styled.a`
   }
 `;
 
-export default () => (
-  <Navbar>
-    <BrandLogo />
-    <NewButton href="https://github.com/devsonket/devsonket.github.io/issues/new?title=%E0%A6%95%E0%A6%BF%E0%A6%B8%E0%A7%87%E0%A6%B0%20%E0%A6%9A%E0%A6%BF%E0%A6%9F%E0%A6%B6%E0%A7%80%E0%A6%9F%20%E0%A6%9A%E0%A6%BE%E0%A6%A8?&body=%E0%A6%95%E0%A6%BF%20%E0%A6%95%E0%A6%BF%20%E0%A6%9A%E0%A6%BE%E0%A6%A8%20%E0%A6%AC%E0%A6%BF%E0%A6%B8%E0%A7%8D%E0%A6%A4%E0%A6%BE%E0%A6%B0%E0%A6%BF%E0%A6%A4%20%E0%A6%B2%E0%A6%BF%E0%A6%96%E0%A7%81%E0%A6%A8&labels=%E0%A6%A8%E0%A6%A4%E0%A7%81%E0%A6%A8%20%E0%A6%86%E0%A6%B0%E0%A7%87%E0%A6%95%E0%A6%9F%E0%A6%BE">
-      + নতুন আরেকটা
-    </NewButton>
-  </Navbar>
-);
+export default class extends PureComponent {
+  intiateAnimation = () => {
+    let latestKnownScrollY = 0,
+      lastScrollY,
+      ticking = false;
+
+    window.addEventListener("scroll", () => {
+      latestKnownScrollY = window.scrollY;
+      requestTick();
+    });
+
+    const update = () => {
+      ticking = false;
+
+      let currentScrollY = latestKnownScrollY - 5;
+
+      if (
+        currentScrollY > lastScrollY &&
+        currentScrollY > 100 &&
+        currentScrollY - lastScrollY > 10
+      ) {
+        this.setState({ show: false });
+      } else if (currentScrollY < lastScrollY) {
+        this.setState({ show: true });
+      }
+
+      lastScrollY = currentScrollY;
+    };
+
+    function requestTick() {
+      if (!ticking) {
+        requestAnimationFrame(update);
+      }
+      ticking = true;
+    }
+  };
+
+  componentDidMount() {
+    this.intiateAnimation();
+  }
+
+  state = {
+    show: true
+  };
+
+  render() {
+    return (
+      <Navbar show={this.state.show}>
+        <BrandLogo />
+        <NewButton href="https://github.com/devsonket/devsonket.github.io/issues/new?title=%E0%A6%95%E0%A6%BF%E0%A6%B8%E0%A7%87%E0%A6%B0%20%E0%A6%9A%E0%A6%BF%E0%A6%9F%E0%A6%B6%E0%A7%80%E0%A6%9F%20%E0%A6%9A%E0%A6%BE%E0%A6%A8?&body=%E0%A6%95%E0%A6%BF%20%E0%A6%95%E0%A6%BF%20%E0%A6%9A%E0%A6%BE%E0%A6%A8%20%E0%A6%AC%E0%A6%BF%E0%A6%B8%E0%A7%8D%E0%A6%A4%E0%A6%BE%E0%A6%B0%E0%A6%BF%E0%A6%A4%20%E0%A6%B2%E0%A6%BF%E0%A6%96%E0%A7%81%E0%A6%A8&labels=%E0%A6%A8%E0%A6%A4%E0%A7%81%E0%A6%A8%20%E0%A6%86%E0%A6%B0%E0%A7%87%E0%A6%95%E0%A6%9F%E0%A6%BE">
+          + নতুন আরেকটা
+        </NewButton>
+      </Navbar>
+    );
+  }
+}
