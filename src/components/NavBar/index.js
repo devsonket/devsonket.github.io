dcfa050e3cf7b73c05da56629f5a128db4d61f96
iamruhul <ruhulamin3482@gmail.com>
2018-12-08 17:04:45 +0600

    refactor(Navbar.js): navbar and it's styles  in one folder

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from "react";
 import styled from "@emotion/styled";
+import "./navbar.css";
 
-import { BrandLogo } from "./BrandLogo";
+import { BrandLogo } from "../BrandLogo";
 
 const Navbar = styled.nav`
   list-style: none;
@@ -10,16 +11,22 @@ const Navbar = styled.nav`
   justify-content: space-between;
   align-items: center;
   padding: 0px 30px;
-  background: rgba(255, 255, 255, 0.94);
   height: 60px;
-  position: fixed;
   z-index: 999;
   width: 100%;
-  border-bottom: 1px solid gainsboro;
   top: 0;
-  z-index: 999;
-  transition: .2s all cubic-bezier(0, 0.94, 1, 0.99);
-  transform: ${props => (props.show ? `translateY(0)` : `translateY(-100%)`)};
+  will-change: transform;
+  position: absolute;
+
+  ${props =>
+    props.changeNav &&
+    `
+      transition: transform 0.2s cubic-bezier(0, 0.94, 1, 0.99);
+      transform: ${props.show ? `translateY(0)` : `translateY(-100%)`};
+      position: fixed;
+      box-shadow: 0 2px 2px -2px rgba(0, 0, 0, 0.15);
+      background: rgba(255, 255, 255, 0.97);
+    `}
 
   @media (max-width: 459px) {
     padding: 0px 10px;
@@ -62,7 +69,7 @@ const NewButton = styled.a`
   transition: 0.2s all ease;
 
   background: #02b3e4;
-  box-shadow: 8px 10px 20px 0 rgba(46, 61, 73, 0.15);
+  box-shadow: 2px 4px 8px 0 rgba(46, 61, 73, 0.2);
   color: white;
   text-shadow: 0 1px 3px #86888e;
 
@@ -71,46 +78,53 @@ const NewButton = styled.a`
   }
 
   &:hover {
-    box-shadow: 2px 4px 8px 0 rgba(46, 61, 73, 0.2);
+    box-shadow: 0px 1px 3px 0 rgba(46, 61, 73, 0.2);
     background-color: #02b7e9;
   }
 `;
 
 export default class extends PureComponent {
   intiateAnimation = () => {
-    let latestKnownScrollY = 0,
-      lastScrollY,
+    let lastScrollTop = 0,
+      latestKnownScrollTop = 0,
       ticking = false;
-
-    window.addEventListener("scroll", () => {
-      latestKnownScrollY = window.scrollY;
-      requestTick();
-    });
 
     const update = () => {
+      // reset the tick
       ticking = false;
 
-      let currentScrollY = latestKnownScrollY - 5;
+      let currScrollTop = latestKnownScrollTop;
+      let isScrollingDown = currScrollTop > lastScrollTop;
+      let isHeaderVisible = currScrollTop < 58;
 
-      if (
-        currentScrollY > lastScrollY &&
-        currentScrollY > 100 &&
-        currentScrollY - lastScrollY > 10
-      ) {
-        this.setState({ show: false });
-      } else if (currentScrollY < lastScrollY) {
-        this.setState({ show: true });
+      if (isScrollingDown && !isHeaderVisible) {
+        this.setState({ show: false }, () => (lastScrollTop = currScrollTop));
+      } else if (!isScrollingDown || !currScrollTop) {
+        this.setState({ show: true }, () => (lastScrollTop = currScrollTop));
       }
 
-      lastScrollY = currentScrollY;
+      if (currScrollTop > 60) {
+        // `is-hidden`  styles are in app.css
+        this.nav.classList.add("is-hidden");
+        setTimeout(() => this.setState({ changeNav: true }), 0);
+      } else if (!currScrollTop) {
+        this.setState({ changeNav: false });
+      }
+    };
+
+    const onScroll = () => {
+      latestKnownScrollTop = window.scrollY;
+      requestTick();
     };
 
     function requestTick() {
       if (!ticking) {
-        requestAnimationFrame(update);
+        window.requestAnimationFrame(update);
       }
       ticking = true;
     }
+
+    window.addEventListener("scroll", onScroll);
   };
 
   componentDidMount() {
@@ -118,12 +132,16 @@ export default class extends PureComponent {
   }
 
   state = {
-    show: true
+    show: false,
+    changeNav: false
   };
 
   render() {
+    const { changeNav, show } = this.state;
+    console.log(show);
+
     return (
-      <Navbar show={this.state.show}>
+      <Navbar ref={e => (this.nav = e)} changeNav={changeNav} show={show}>
         <BrandLogo />
         <NewButton href="https://github.com/devsonket/devsonket.github.io/issues/new?title=%E0%A6%95%E0%A6%BF%E0%A6%B8%E0%A7%87%E0%A6%B0%20%E0%A6%9A%E0%A6%BF%E0%A6%9F%E0%A6%B6%E0%A7%80%E0%A6%9F%20%E0%A6%9A%E0%A6%BE%E0%A6%A8?&body=%E0%A6%95%E0%A6%BF%20%E0%A6%95%E0%A6%BF%20%E0%A6%9A%E0%A6%BE%E0%A6%A8%20%E0%A6%AC%E0%A6%BF%E0%A6%B8%E0%A7%8D%E0%A6%A4%E0%A6%BE%E0%A6%B0%E0%A6%BF%E0%A6%A4%20%E0%A6%B2%E0%A6%BF%E0%A6%96%E0%A7%81%E0%A6%A8&labels=%E0%A6%A8%E0%A6%A4%E0%A7%81%E0%A6%A8%20%E0%A6%86%E0%A6%B0%E0%A7%87%E0%A6%95%E0%A6%9F%E0%A6%BE">
           + নতুন আরেকটা
