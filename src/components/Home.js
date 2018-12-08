ef4d3f9c6478ecf4ebbef2855570097583052992
iamruhul <ruhulamin3482@gmail.com>
2018-12-08 17:05:02 +0600

    feature(navbar-redesign): complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,13 @@ import TopCards from "./TopCards";
 import Contents from "./Contents";
 import Footer from "./Footer";
 import MastHead from "./mastHead";
-import NavBar from "./NavBar";
+import Navbar from "./NavBar";
 
 export default ({ searchAItem, searchResult, tops, data }) => {
   return (
     <React.Fragment>
       {/* <HeaderHome searchAItem={searchAItem} /> */}
-      <NavBar />
+      <Navbar />
       <MastHead />
       <Container>
         <TopCards tops={searchResult ? searchResult : tops} />
