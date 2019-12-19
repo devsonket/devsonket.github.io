658d3bd8f3a05226acea41ccfa5f51a114fbca69
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-12-19 20:43:45 +0600

    search not found link

--- src/components/searchresult.js
+++ src/components/searchresult.js
@@ -75,6 +75,7 @@ const SearchResult = () => {
       <input
         onChange={e => setInput(e.target.value)}
         onFocus={() => setActive(true)}
+        onBlur={() => setTimeout(() => setActive(false), 300)}
         value={input}
         placeholder="কিসের উপর চিটশিট চাচ্ছেন?"
       />
@@ -125,7 +126,23 @@ const SearchResult = () => {
               }
               const data = matchNums.map(one => edges[one])
               if (!data.length) {
-                return <div>Request for new cheatsheet</div>
+                return (
+                  <li className="not-found">
+                    <a
+                      target="__blank"
+                      rel="noopener noreferrer"
+                      href="https://github.com/devsonket/devsonket.github.io/issues/new?title=%E0%A6%95%E0%A6%BF%E0%A6%B8%E0%A7%87%E0%A6%B0%20%E0%A6%9A%E0%A6%BF%E0%A6%9F%E0%A6%B6%E0%A7%80%E0%A6%9F%20%E0%A6%9A%E0%A6%BE%E0%A6%A8?&body=%E0%A6%95%E0%A6%BF%20%E0%A6%95%E0%A6%BF%20%E0%A6%9A%E0%A6%BE%E0%A6%A8%20%E0%A6%AC%E0%A6%BF%E0%A6%B8%E0%A7%8D%E0%A6%A4%E0%A6%BE%E0%A6%B0%E0%A6%BF%E0%A6%A4%20%E0%A6%B2%E0%A6%BF%E0%A6%96%E0%A7%81%E0%A6%A8&labels=%E0%A6%A8%E0%A6%A4%E0%A7%81%E0%A6%A8%20%E0%A6%86%E0%A6%B0%E0%A7%87%E0%A6%95%E0%A6%9F%E0%A6%BE"
+                    >
+                      কোন চিটশিট পাওয়া যায়নি
+                      <p>
+                        <small>
+                          <strong>{input}</strong> এর উপর চিটশিট রিকোয়েস্ট করুন
+                          অথবা চাইলে নিজেও কন্ট্রিবিউট করতে পারেন
+                        </small>
+                      </p>
+                    </a>
+                  </li>
+                )
               }
               return input.length
                 ? data.map(edge => {
