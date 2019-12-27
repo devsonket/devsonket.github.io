de0c7069bb1c919b3cfc798d42e7098991f44f83
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-12-27 20:37:04 +0600

    contributors fix

--- src/components/contributors.js
+++ src/components/contributors.js
@@ -17,6 +17,7 @@ const ContributorsContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-wrap: wrap;
     list-style: none;
     li {
       height: 40px;
